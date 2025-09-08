2f982cb44662d9e0dc639c1a7bbd2f83fe5b2cba
ahmet21ahmet <53349631+ahmet21ahmet@users.noreply.github.com>
2025-09-08 12:08:11 +0300

    Update a.py

--- a.py
+++ a.py
@@ -1,7 +1,7 @@
 import asyncio
 from playwright.async_api import async_playwright
 
-URL = "https://www.dizibox.live/"
+URL = "https://streambtw.com"
 
 async def main():
     async with async_playwright() as p:
